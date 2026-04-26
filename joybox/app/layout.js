286e766a427abc52611fa94958bef9108ce20664
Yan Kai <joybox9794@gmail.com>
2026-04-26 16:58:03 +0800

    update favicon

--- joybox/app/layout.js
+++ joybox/app/layout.js
@@ -14,6 +14,9 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "JoyBox SG",
   description: "Stress-free birthday event planning in Singapore",
+  icons: {
+    icon: "/favicon.ico",
+    },
 };
 
 export default function RootLayout({ children }) {
